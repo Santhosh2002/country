72f5908573bbb2746fe12f4325f43848792f0bb0
Santhosh2002 <202051068@iiitvadodara.ac.in>
2023-04-16 18:49:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,40 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
+
 function App() {
-  const [data, setData] = useState();
-  const [name, setName] = useState();
-  const [capital, setCapital] = useState();
-  const [currency, setCurrency] = useState();
-  const [population, setPopulation] = useState();
-  var list = [
-    {
-      name: name,
-      capital: capital,
-      currency: currency,
-      population: population,
-    },
-  ];
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    const Data = () => {
-      fetch("https://restcountries.com/v3.1/all")
-        .then((response) =>
-          response.json().then((data, index) => {
-            setData(data);
-            setName(data[0].name.common);
-            setCapital(data[0].capital);
-            setCurrency(data[0].currencies.BBD.name);
-            setPopulation(data[0].population);
-          })
-        )
-        .catch((error) => console.log(error));
-    };
-    Data();
-  }, [name]);
-  console.log(data);
-  const listItems = list.map((data) => (
-    <tr>
-      <td>{data.name}</td>
-      <td>{data.capital}</td>
-      <td>{data.currency}</td>
-      <td>{data.population}</td>
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const listItems = data.map((item, index) => (
+    <tr key={index}>
+      <td>{item.name.common}</td>
+      <td>{item.capital && item.capital[0]}</td>
+      <td>
+        {item.currencies &&
+          item.currencies[Object.keys(item.currencies)[0]].name}
+      </td>
+      <td>{item.population}</td>
     </tr>
   ));
+
   return (
     <div className="App">
       <table>
-        <tr>
-          <th>Country Name</th>
-          <th>Captial</th>
-          <th>currency</th>
-          <th>Population</th>
-        </tr>
-        {listItems}
+        <thead>
+          <tr>
+            <th>Country Name</th>
+            <th>Captial</th>
+            <th>Currency</th>
+            <th>Population</th>
+          </tr>
+        </thead>
+        <tbody>{listItems}</tbody>
       </table>
     </div>
   );
